7430490e6b1c9eedd8f83d77a18a78c16f2158e5
kainklob <klobk23@gmail.com>
2022-08-02 14:04:06 -0400

    ini commit. edited default app + added app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("it renders without crashing", function () {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test("it matches the snapshot", function () {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
 });
